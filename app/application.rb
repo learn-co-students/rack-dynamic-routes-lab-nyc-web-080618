85673cccf62b9c5c7a4f2f86d1fa0e36d65fb105
github username <github email address>
2018-08-27 13:56:23 -0400

    Done

---
+++ app/application.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      # binding.pry
+      search_term = req.path_info.split("/")[2]
+      my_item = Item.all.find do |item|
+        item.name == search_term
+      end
+      if my_item
+        resp.write "#{my_item.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+  def handle_search(search_term)
+    if @@items.include?(search_term)
+      return "#{search_term} is one of our items"
+    else
+      return "Couldn't find #{search_term}"
+    end
+  end
+end
